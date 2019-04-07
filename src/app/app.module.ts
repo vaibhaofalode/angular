be50182969f3a33905135ff747fe0d4da727feeb
Vaibhao Falode <vaibhao.falode@tudip.nl>
2019-04-07 23:56:23 +0530

    Update:
    Details:
    - Added font awesome in the project.
    - Added spinner to the project.
    - Created components for register page, login page and index page.
    - Added shared navbar component to the project.
    - Added routing to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { RouterModule } from '@angular/router';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { IndexComponent } from './index/index.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    UserDetailsComponent,
+    NavbarComponent,
+    IndexComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: IndexComponent
+      },
+      {
+        path: 'users',
+        component: HomepageComponent
+     },
+     {
+      path: 'users/register',
+      component: RegisterComponent
+     },
+     {
+      path: 'users/login',
+      component: LoginComponent
+     },
+     {
+      path: 'users/:userId',
+      component: UserDetailsComponent
+     },
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-homepage',
@@ -6,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  users: any;
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient, private spinner: NgxSpinnerService) {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.spinner.show();
+    this.httpClient.get('https://reqres.in/api/users?per_page=10').subscribe(
+      (data) => this.getDataSuccess(data),
+      (error) => this.getDataError(error)
+    );
+  }
+  getDataSuccess(data) {
+    if(data && data.data) {
+      this.users = data.data
+    }
+    this.spinner.hide();
+  }
+
+  getDataError(error) {
+    this.spinner.hide();    
+  }
 
   ngOnInit() {
   }
